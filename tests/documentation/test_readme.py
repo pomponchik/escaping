720d9ad126ccf63d16065ec878e9061fc1cbd616
Evgeniy Blinov <zheni-b@yandex.ru>
2024-04-09 17:56:36 +0300

    +1 test

--- tests/documentation/test_readme.py
+++ tests/documentation/test_readme.py
@@ -4,11 +4,12 @@ from io import StringIO
 
 import pytest
 import full_match
+from emptylog import MemoryLogger
 
 import escape
 
 
-def test_quick_start():
+def test_example_quick_start():
     @escape
     def function():
         raise ValueError
@@ -16,7 +17,7 @@ def test_quick_start():
     function()  # The exception is suppressed.
 
 
-def test_decorator_mode_passing_exceptions():
+def test_example_decorator_mode_passing_exceptions():
     @escape(ValueError, ZeroDivisionError)
     def function():
         raise ValueError('oh!')
@@ -29,7 +30,7 @@ def test_decorator_mode_passing_exceptions():
     asyncio.run(async_function())  # Silence.
 
 
-def test_decorator_mode_not_suppressing_exception():
+def test_example_decorator_mode_not_suppressing_exception():
     @escape()
     def function():
         raise ValueError('oh!')
@@ -39,7 +40,7 @@ def test_decorator_mode_not_suppressing_exception():
         # > ValueError: oh!
 
 
-def test_decorator_mode_default_value():
+def test_example_decorator_mode_default_value():
     @escape(ValueError, default='some value')
     def function():
         raise ValueError
@@ -47,7 +48,7 @@ def test_decorator_mode_default_value():
     assert function() == 'some value'  # It's going to work.
 
 
-def test_decorator_mode_with_empty_breackets():
+def test_example_decorator_mode_with_empty_breackets():
     @escape
     def function():
         raise ValueError
@@ -55,7 +56,7 @@ def test_decorator_mode_with_empty_breackets():
     function()  # Silence still.
 
 
-def test_decorator_more_equivalents():
+def test_example_decorator_more_equivalents():
     @escape(...)
     def function_1():
         raise ValueError
@@ -68,7 +69,7 @@ def test_decorator_more_equivalents():
     function_2()  # These two functions are completely equivalent.
 
 
-def test_decorator_mode_exception_and_ellipsis_separated_by_comma():
+def test_example_decorator_mode_exception_and_ellipsis_separated_by_comma():
     @escape(GeneratorExit, ...)
     def function():
         raise GeneratorExit
@@ -76,7 +77,7 @@ def test_decorator_mode_exception_and_ellipsis_separated_by_comma():
     function()
 
 
-def test_context_manager_basic_examples():
+def test_example_context_manager_basic_examples():
     with escape(ValueError):
         raise ValueError
 
@@ -87,13 +88,13 @@ def test_context_manager_basic_examples():
         raise ValueError
 
 
-def test_context_manager_attempt_to_set_default_value():
+def test_example_context_manager_attempt_to_set_default_value():
     with pytest.raises(escape.errors.SetDefaultReturnValueForContextManagerError, match=full_match('You cannot set a default value for the context manager. This is only possible for the decorator.')):
         with escape(default='some value'):
             ...
 
 
-def test_callbacks_simple_success_callback():
+def test_example_callbacks_simple_success_callback():
     buffer = StringIO()
 
     with redirect_stdout(buffer):
@@ -101,3 +102,13 @@ def test_callbacks_simple_success_callback():
             pass
 
     assert buffer.getvalue() == 'The code block ended without errors.\n'
+
+
+def test_example_success_logging_on_in_context_manager():
+    logger = MemoryLogger()
+
+    with escape(success_logging=True, logger=logger):
+        pass
+
+    assert len(logger.data) == 1
+    assert logger.data.info[0].message == 'The code block was executed successfully.'
